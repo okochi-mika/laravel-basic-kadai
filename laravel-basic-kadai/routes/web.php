4b0426df5f0a3daec08ec65278f644712dc29b86
okochi-mika <hiyoko_m@icloud.com>
2025-04-24 17:57:03 +0900

    コードを修正

--- laravel-basic-kadai/routes/web.php
+++ laravel-basic-kadai/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController; // ← これ大事！
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts', [PostController::class, 'index']); // ← これもOK
