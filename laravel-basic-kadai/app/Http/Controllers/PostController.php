d0c9995d831e77b1cc1516e472b13934b682fb37
okochi-mika <hiyoko_m@icloud.com>
2025-04-25 01:22:30 +0900

    コードの変更

--- laravel-basic-kadai/app/Http/Controllers/PostController.php
+++ laravel-basic-kadai/app/Http/Controllers/PostController.php
@@ -1,13 +1,18 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        // postsテーブルからすべてのデータを取得
+        $posts = DB::table('posts')->get();
+
+        // ビューにデータを渡す
+        return view('posts.index', ['posts' => $posts]);
     }
 }
-
--- laravel-basic-kadai/resources/views/posts/index.blade.php
+++ laravel-basic-kadai/resources/views/posts/index.blade.php
@@ -9,6 +9,14 @@
 
 <body>
     <h1>投稿一覧</h1>
+
+    @foreach ($posts as $post)
+        <div style="margin-bottom: 20px;">
+            <h2>{{ $post->title }}</h2>
+            <p>{{ $post->content }}</p>
+        </div>
+    @endforeach
+    
 </body>
 
 </html>
